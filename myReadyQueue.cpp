dec072521f3a013b9942dc629edb76cee55f0f6e
Patricia Figueroa <patricia.figueroa@ucdenver.edu>
2017-10-29 11:52:27 -0600

    finished ready queue and PCD

--- myReadyQueue.cpp
+++ myReadyQueue.cpp
@@ -2,4 +2,37 @@
 // Created by Patricia on 10/12/17.
 //
 
+#include <iostream>
 #include "myReadyQueue.h"
+
+using namespace std;
+
+ReadyQueue::ReadyQueue() {
+    size = 0;
+    head = nullptr;
+    tail = nullptr;
+}
+
+void ReadyQueue::enqueue(Process *process) {
+    if(size == 0){
+        head = process;
+        tail = process;
+    }
+    else{
+        tail->next = process;
+        tail = process;
+    }
+    size++;
+}
+
+Process* ReadyQueue::dequeue() {
+    if(size <= 0){
+        return nullptr;
+    }
+    else{
+        Process *temp = head;
+        head = head->next;
+        size--;
+        return temp;
+    }
+}
--- myReadyQueue.h
+++ myReadyQueue.h
@@ -5,13 +5,22 @@
 #ifndef MYREADYQUEUE_H
 #define MYREADYQUEUE_H
 
+#include "myProcess.h"
 
+//This class is a custom queue which holds data of Process type
+class ReadyQueue{
+public:
+    //Size of the queue
+    int size;
+    Process *head;
+    Process *tail;
 
+    ReadyQueue();
+    //Method to add process to the queue
+    void enqueue(Process *process);
+    //Method to remove process (tail) from the queue
+    Process* dequeue();
 
-
-
-
-
-
+};
 
 #endif //MYREADYQUEUE_H
